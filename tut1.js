f2d456784d63abfa6bad9aa1859913c34e167952
Jake <j.dunn15@imperial.ac.uk>
2019-04-06 18:08:10 +0100

    Basic Model trained. Time to experiment!

--- tut1.js
+++ tut1.js
@@ -34,6 +34,13 @@ const run = async () => {
 
     const model = createModel();
     tfvis.show.modelSummary({name: 'Model Summary'}, model);
+    const tensorData = convertToTensor(data);
+    const {inputs, labels} = tensorData;
+    //train the model
+    await trainModel(model, inputs, labels);
+    console.log('Training Complete');
+    //make some predictions
+    testModel(model, data, tensorData);
 };
 
 const createModel = () => {
@@ -80,4 +87,73 @@ const convertToTensor = (data) => {
         }
     });
 };
+
+const trainModel = async (model, inputs, labels) => {
+    //prepare model for training
+    model.compile({
+        optimizer: tf.train.adam(),
+        loss: tf.losses.meanSquaredError,
+        metrics: ['mse']
+    });
+
+    const batchSize = 28;
+    const epochs = 50;
+
+    return await model.fit(inputs, labels, {
+        batchSize,
+        epochs,
+        shuffle: true,
+        callbacks: tfvis.show.fitCallbacks(
+            {name: 'Training Performance '},
+            ['loss', 'mse'],
+            {height: 200, callbacks: ['onEpochEnd']}
+        )
+    });
+};
+
+function testModel(model, inputData, normalizationData) {
+    const {inputMax, inputMin, labelMin, labelMax} = normalizationData;
+
+    // Generate predictions for a uniform range of numbers between 0 and 1;
+    // We un-normalize the data by doing the inverse of the min-max scaling
+    // that we did earlier.
+    const [xs, preds] = tf.tidy(() => {
+
+        const xs = tf.linspace(0, 1, 100);
+        const preds = model.predict(xs.reshape([100, 1]));
+
+        const unNormXs = xs
+            .mul(inputMax.sub(inputMin))
+            .add(inputMin);
+
+        const unNormPreds = preds
+            .mul(labelMax.sub(labelMin))
+            .add(labelMin);
+
+        // Un-normalize the data
+        return [unNormXs.dataSync(), unNormPreds.dataSync()];
+    });
+
+
+    const predictedPoints = Array.from(xs).map((val, i) => {
+        return {x: val, y: preds[i]}
+    });
+
+    const originalPoints = inputData.map(d => ({
+        x: d.horsepower, y: d.mpg,
+    }));
+
+
+    tfvis.render.scatterplot(
+        {name: 'Model Predictions vs Original Data'},
+        {values: [originalPoints, predictedPoints], series: ['original', 'predicted']},
+        {
+            xLabel: 'Horsepower',
+            yLabel: 'MPG',
+            height: 300
+        }
+    );
+}
+
+
 document.addEventListener('DOMContentLoaded', run);
